c7edf262c356bbd263f27519deeff7b824496bcb
kelvinjou <kelvin.jou@gmail.com>
2021-03-06 19:05:41 -0800

    Initial Commit

---
+++ RealmStorageDemo/RealmStorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmStorageDemoApp.swift
+//  RealmStorageDemo
+//
+//  Created by mimi on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct RealmStorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
